6c71db195345906242cbe56c6327216d223eb419
ShaikhZuber08 <90678325+ShaikhJuber@users.noreply.github.com>
2022-09-22 12:50:12 +0530

    first commit by upload

---
+++ reddit clone/script.js
@@ -0,0 +1,10 @@
+//// Scroll Button to fixed Navbar and change background color  //////
+
+const navbar = document.querySelector('.navbar');
+window.onscroll = () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add('nav-active');
+    } else {
+        navbar.classList.remove('nav-active');
+    }
+};
